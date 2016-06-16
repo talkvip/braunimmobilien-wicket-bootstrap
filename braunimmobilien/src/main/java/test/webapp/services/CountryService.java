d854952bd5c779f7f60754327d748ba5cb330dc8
braunimmobilien <h.h.braun@ich-ueberall.de>
2016-06-16 17:50:36 +0200

    Update CountryService.java

--- braunimmobilien/src/main/java/test/webapp/services/CountryService.java
+++ braunimmobilien/src/main/java/test/webapp/services/CountryService.java
@@ -1,1 +1,20 @@
 
+package test.webapp.services;
+
+import test.webapp.pages.components.country.Country;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Service providing countries for drop down country list.
+ *
+ * @author Marcin Zajączkowski, 2013-11-18
+ */
+@Deprecated //not needed with SimpleCountryDropDownChoice
+public interface CountryService {
+
+    List<Country> getAvailableCountriesInLocale(Locale locale);
+
+    Country getCountryInLocaleByCode(Locale locale, String code);
+}
