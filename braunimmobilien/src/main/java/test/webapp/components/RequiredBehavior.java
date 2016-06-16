436acbd5e67410b6ec3c9a38aeb4088c644e8f23
braunimmobilien <h.h.braun@ich-ueberall.de>
2016-06-16 18:01:29 +0200

    Add files via upload

--- braunimmobilien/src/main/java/test/webapp/components/RequiredBehavior.java
+++ braunimmobilien/src/main/java/test/webapp/components/RequiredBehavior.java
@@ -5,13 +5,13 @@ import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.ComponentTag;
 
 /**
- * A behavior that adds a autofocus attribute support for an input components.
+ * A behavior that adds a required attribute support for an input components.
  */
-public class AutofocusBehavior extends Behavior {
+public class RequiredBehavior extends Behavior {
 
     @Override
     public void onComponentTag(Component component, ComponentTag tag) {
         super.onComponentTag(component, tag);
-        tag.put("autofocus", "autofocus");
+        tag.put("required", "required");
     }
 }
